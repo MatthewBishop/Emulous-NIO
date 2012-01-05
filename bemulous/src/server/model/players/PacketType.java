3ec2db6c39f8a784498d399561d958387b03c2d7
Advocatus <davidcntt@hotmail.com>
2012-01-05 13:41:47 -0500

    Gitignore fixed

--- bemulous/src/server/model/players/PacketType.java
+++
@@ -1,9 +0,0 @@
-package server.model.players;
-
-import server.model.players.packets.*;
-
-	
-public interface PacketType {
-	public void processPacket(Client c, int packetType, int packetSize);
-}
-
